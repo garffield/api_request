542e118e4eb6fea5ef6ca1cde58570142be6f6cc
garffield <gabrielfranklinbarcellos@gmail.com>
2025-01-27 21:04:03 -0400

    a

---
+++ app.js
@@ -0,0 +1,9 @@
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+  });
+
+app.listen(port, () => {
+    console.log(`Servidor rodando na porta ${port}`);
+  });

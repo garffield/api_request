44a50228b35dd29e09629d950b80b6e0fac6f982
garffield <gabrielfranklinbarcellos@gmail.com>
2025-01-28 17:13:29 -0400

    requisição feita

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
-const app = express();
+const pokeID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 493]
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
+const pokemomArray = []
 
-app.listen(port, () => {
-    console.log(`Servidor rodando na porta ${port}`);
-  });
+async function apiCall() {
+  
+  try { 
+ 
+    for (let i of pokeID) {
+      
+      let resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+      
+      const data = await resp.json();
+      
+      pokemomArray.push({
+        imageP: data.sprites.front_default,
+        name: data.name,
+        type: data.types[0].type.name,
+        baseExperience: data.base_experience,
+        height: data.height,
+        weight: data.weight
+        }
+      );
+    }
+    
+  }  
+    catch (error) {
+    console.error(`Erro Gerado: ${error}`)
+  }
+
+}
+  
+
+
+
+
+apiCall();

0733eaefeab7a8e191ce359ceb10fb355aaa4524
garffield <gabrielfranklinbarcellos@gmail.com>
2025-01-29 01:54:50 -0400

    logica implementada

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const pokeID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 493]
-
-const pokemomArray = []
-
 async function apiCall() {
-  
-  try { 
- 
+
+  try {
+
+    const pokeID = [1, 2, 3, 4, 5, 6, 7, 8, 9, 493]
+
+    const result = document.getElementById("pokeList");
+    result.innerHTML = '';
+    let pokeList = ''
+
+    const pokemomArray = []
+
     for (let i of pokeID) {
-      
+
       let resp = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
-      
+
       const data = await resp.json();
-      
+
       pokemomArray.push({
         imageP: data.sprites.front_default,
         name: data.name,
@@ -19,19 +23,34 @@ async function apiCall() {
         baseExperience: data.base_experience,
         height: data.height,
         weight: data.weight
-        }
-      );
+      });
     }
-    
-  }  
-    catch (error) {
-    console.error(`Erro Gerado: ${error}`)
+
+
+    let pokeIndex = 0
+    pokemomArray.forEach(e => {
+
+      pokeIndex = pokemomArray.indexOf(e)
+
+      pokeList += `<div class="cardPokemon">
+                    <img src="${e.imageP}" alt="${e.name}">
+                    <p>Nome: ${e.name}</p>
+                    <p>Tipo: ${e.type}</p>
+                    <p>Base Experience: ${e.baseExperience}</p>
+                    <p>Peso: ${e.weight}</p>
+                    <p>Altura: ${e.height}</p>
+                    </div>
+                  `
+
+    });
+
+    result.innerHTML = pokeList ;
+
   }
 
+  catch (error) {
+    console.error(`Erro Gerado: ${error}`)
+  }
 }
-  
 
-
-
-
-apiCall();
+apiCall()
